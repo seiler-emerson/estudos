c71060b69d0b2872dfe2b2fe028003bef92209cb
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-10-30 12:27:20 -0300

    estudos javascript

---
+++ programadorbr/ex002/script.js
@@ -0,0 +1,15 @@
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+
+var a = "String";  //String = Comporta uma cadeia de caracter
+var b = 1;         //Number = Comporta um número
+var c = null;      //Null = Variavel vazia
+var d = true;      //Boolean  = comporta um valor verdadeiro ou falso
+var e = false      //Boolean  = comporta um valor verdadeiro ou falso
+   
+
+
+console.log();
