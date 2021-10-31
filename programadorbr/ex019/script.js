992041378ac0d196a5c6705ae8a1a60bd3a4fbe5
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-10-31 16:37:35 -0300

    aulas javascript

---
+++ programadorbr/ex019/script.js
@@ -0,0 +1,20 @@
+
+
+function media(n1,n2){
+var nota1 = n1;
+var nota2 = n2;
+var media = (nota1 + nota2) /2 ;
+
+
+return media;
+}
+
+
+
+var resultado = media(6,7)
+var m = media;
+var resultado2 = m(2,3)
+
+
+console.log(resultado)
+console.log(resultado2)
