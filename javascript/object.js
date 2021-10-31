e449bb1fb98a9a4ff4ff4ebcc30862bb05fbe073
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-10-31 20:56:25 -0300

    estudos

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+/* 
+
+* Object
+
+ - Dado estrutural
+ - Objeto
+ - Propriedades / Atributos
+ - Funcionalidades / Métodos
+
+  {propriedade: "valor"}
+*/
+
+
+console.log({
+  name: "Emerson",
+  idade: 26,
+  andar: function() {
+    console.log('andar') 
+  }
+})  
