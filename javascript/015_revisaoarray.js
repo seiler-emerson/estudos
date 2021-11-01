475f1dbf36bf57d466128cb9800e10f54028f2fd
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-11-01 15:29:52 -0300

    estudos

---
+++ javascript/015_revisaoarray.js
@@ -0,0 +1,16 @@
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat',
+    {
+      name: 'Dog',
+      age: 3
+    }
+]
+
+//acessar valores dentro do array
+console.log(animals[1])
+console.log(animals[3].name)
+
