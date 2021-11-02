f2a8f3899a39e6d61e90d562ed1c742cba69f582
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-11-02 11:07:28 -0300

    studies

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,49 @@
-// Array
+let weight
 
-const animals = [
-    'Lion',
-    'Monkey',
-    'Cat',
-    {
-      name: 'Dog',
-      age: 3
-    }
+console.log(typeof weight)
+
+var datas = {
+    name: "Emerson",
+    age: 26,
+    stars: 5.8,
+    isSubscribed: true
+
+}
+
+console.log(datas.name)
+console.log(datas.age)
+console.log(datas.stars)
+console.log(datas.isSubscribed)
+
+var student = {
+  name: "Emerson",
+    age: 26,
+    stars: 5.8,
+    isSubscribed: true
+}
+
+console.log(`${student.name} de idade ${student.age} anos e ${student.stars} pelos valores de propriedade do objeto`)
+
+
+students = [
+  student
 ]
 
-//acessar valores dentro do array
-console.log(animals[1])
-console.log(animals[3].name)
+console.log(students)
+console.log(students[0])
 
+const john = {
+  name: "john",
+  age: 23,
+  weight: 74.8,
+  isSubscribed: true
+}
+
+students = [
+  student,
+  john
+]
+console.log(students)
+
+students[1] = john
+console.log(students)
