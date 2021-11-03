81416381b2c7b624009ef261e2812e66ac09496d
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-11-03 09:14:21 -0300

    studies

---
+++ javascript/functions/function_01.js
@@ -0,0 +1,12 @@
+// criar um aplicativo de frases motivacionais
+
+//Function Declaration
+function createPhrases() {
+  console.log('Estudar é muito bom')
+  console.log('Paciência e persistência')
+  console.log('Revisão é a mãe do aprendizado')
+}
+
+
+//Execute, run, call, invoke
+createPhrases()
---
+++ javascript/functions/function_02.js
@@ -0,0 +1,17 @@
+//function expression
+//function anonymous
+
+//Parameters
+const sum = function(number1, number2){
+  total = number1 + number2
+  return total
+}
+
+
+let number1 = 34
+let number2 = 25
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${sum(number1, number2)}`)
+
