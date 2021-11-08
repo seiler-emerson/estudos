63ddd622762e38bf612a4fe7ef9b6b1ab4a69b2c
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-11-08 14:36:26 -0300

    Update script04.js

--- javascript/exercices/script04.js
+++ javascript/exercices/script04.js
@@ -47,7 +47,42 @@ const booksByCategory = [
   }
 ]
 
-let numberOfCategory = booksByCategory.length
+const totalCategory = booksByCategory.length
 
-console.log(`O número de categoria de livros é ${numberOfCategory}`)
+console.log(totalCategory)
+for(let category of booksByCategory) {
+  console.log(`Total de livros da categoria: `, category.category)
+  console.log(category.books.length)
+}
+
+function countAuthors() {
+  let authors = [];
+
+  for(let category of booksByCategory) {
+    for(let book of category.books) {
+      if(authors.indexOf(book.author) == -1) {
+        authors.push(book.authors)
+      }
+    }
+  }
+
+  console.log("Total de autores: ", authors.length)
+}
+
+countAuthors();
+
+function booksOfAuthor(author) {
+  let books = [];
+
+  for(let category of booksByCategory) {
+    for(let book of category.books) {
+      if(book.author === author) {
+        books.push(book.title)
+      }
+      }
+    }
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+}
+
+booksOfAuthor('Augusto Cury');
 
