d04ea21d2e86b253228f1a6d1cf7a12173c16e25
seileremerson <42720635+seileremerson@users.noreply.github.com>
2021-11-16 09:25:09 -0300

    estudos

---
+++ programadorbr/ex027/script.js
@@ -0,0 +1,10 @@
+var d = new Date(2021, 10, 16);
+var d1 = new Date("sep 05 2017 23:25");
+var d2 = new Date("10 05 2017 23:25");
+var d3 = new Date("04/08/1995");
+console.log(d)
+console.log(d1)
+console.log(d2)
+console.log(d3)
+console.log(d1.getFullYear())
+console.log(d1.getDate())
