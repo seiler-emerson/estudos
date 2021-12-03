88ccad0673343c6e4669cd0ec0d5cfe9741fe7d1
seileremerson <seiler.emerson@gmail.com>
2021-12-03 16:37:38 -0300

    estudos

--- curso_em_video/javascript/aula11/ex009.js
+++
@@ -1,6 +0,0 @@
-var pais = 'EUA'
-if (pais == 'Brasil') {
-  console.log('Brasileiro')
-} else {
-  console.log('Estrangeiro')
-}
--- curso_em_video/javascript/aula12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 68
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-  console.log(`Não vota`)
-} else if (idade < 18 || idade > 65) {
-  console.log(`Voto Opcional`)
-} else {
-  console.log(`Voto Obrigatório`)
-}
