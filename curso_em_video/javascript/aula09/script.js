4f6b512a5812d87cd4e1b7da0611019c9b4b5518
seileremerson <seiler.emerson@gmail.com>
2021-12-03 16:20:26 -0300

    estudos

---
+++ curso_em_video/javascript/aula09/script.js
@@ -0,0 +1,8 @@
+<script>
+    
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0];
+var d = window.document.querySelector('div#msg');
+d.style.background = 'blue'
+
+</script>
