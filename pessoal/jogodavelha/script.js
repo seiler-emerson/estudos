af05ce91eb0746b6d78054b8d9a3c64328c2e171
seileremerson <seiler.emerson@gmail.com>
2021-12-19 09:09:08 -0300

    estudos

---
+++ pessoal/jogodavelha/script.js
@@ -0,0 +1,27 @@
+function circulo1() {
+    document.querySelectorAll('.item')[0].classList.add('circulo')
+}
+function circulo2() {
+    document.querySelectorAll('.item')[1].classList.add('circulo')
+}
+function circulo3() {
+    document.querySelectorAll('.item')[2].classList.add('circulo')
+}
+function circulo4() {
+    document.querySelectorAll('.item')[3].classList.add('circulo')
+}
+function circulo5() {
+    document.querySelectorAll('.item')[4].classList.add('circulo')
+}
+function circulo6() {
+    document.querySelectorAll('.item')[5].classList.add('circulo')
+}
+function circulo7() {
+    document.querySelectorAll('.item')[6].classList.add('circulo')
+}
+function circulo8() {
+    document.querySelectorAll('.item')[7].classList.add('circulo')
+}
+function circulo9() {
+    document.querySelectorAll('.item')[8].classList.add('circulo')
+}
