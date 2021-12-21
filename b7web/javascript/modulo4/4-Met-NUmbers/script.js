92e11f77473bbd31640010551140c7736dc4e8fc
seileremerson <seiler.emerson@gmail.com>
2021-12-21 14:58:31 -0300

    estudos

---
+++ b7web/javascript/modulo4/4-Met-NUmbers/script.js
@@ -0,0 +1,8 @@
+let n = '25.6';
+
+//let res = n.toString();
+//let res = n.toFixed(2);
+//let res = parseInt(n);
+let res = parseFloat(n);
+
+console.log(res); 
---
+++ b7web/javascript/modulo4/6-Met-Arrays-2/script.js
@@ -0,0 +1,13 @@
+let lista = ['Ovo','Farinha','Corante','Massa'];
+// let lista2 = ['Prato','Liquidificador','Forno'];
+
+//delete lista[1];
+// lista.splice(1,2);
+// let res = lista.concat(lista2);
+
+lista.sort();
+lista.reverse();
+
+let res = lista;
+
+console.log(res); 
