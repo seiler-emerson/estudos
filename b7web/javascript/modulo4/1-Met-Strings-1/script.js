74c0df9db9d21b65228a1a66ef344cb60a14e185
seileremerson <seiler.emerson@gmail.com>
2021-12-21 12:43:48 -0300

    estudos

---
+++ b7web/javascript/modulo4/1-Met-Strings-1/script.js
@@ -0,0 +1,10 @@
+let nome = "Emerson Seiler";
+
+let resultado = nome.length;
+let resultado1 = nome.indexOf('Seiler');
+
+
+
+
+
+console.log(resultado1);
---
+++ b7web/javascript/modulo4/3-Met-Strings-3/script.js
@@ -0,0 +1,11 @@
+let nome = "Emerson Seiler";
+
+let resultado1 = nome.replace('Seiler', 'Francesconi');
+let resultado2 = nome.toUpperCase();
+let resultado3 = nome.toLowerCase();
+let resultado4 = nome.concat(' ','Emerson');
+let resultado5 = nome.trim();
+let resultado6 = nome.charAt(6);
+let resultado = nome.split(' ');
+
+console.log(resultado); 
