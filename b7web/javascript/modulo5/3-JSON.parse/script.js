e6f5a8a5eb899de037545247d0342c949b6a3883
seileremerson <seiler.emerson@gmail.com>
2021-12-24 13:50:56 -0300

    estudos

---
+++ b7web/javascript/modulo5/3-JSON.parse/script.js
@@ -0,0 +1,3 @@
+let pessoa = JSON.parse('{"nome": "seiler", "idade": 26}');
+
+console.log(pessoa);
---
+++ b7web/javascript/modulo5/4-JSON.stringify/script.js
@@ -0,0 +1,4 @@
+let pessoa = JSON.stringify({nome: "seiler", idade: 26}); //Transforma JSON EM STRING
+
+console.log(pessoa);
+
