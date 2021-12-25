9ca84e4d62c5a489152bcb02dbe9d8923126699f
seileremerson <seiler.emerson@gmail.com>
2021-12-25 08:51:17 -0300

    estudos

---
+++ script.js
@@ -0,0 +1,20 @@
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        c('.pizzaWindowArea').style.display = 'flex';
+    })
+      
+    c('.pizza-area').append( pizzaItem );
+});
+
+
