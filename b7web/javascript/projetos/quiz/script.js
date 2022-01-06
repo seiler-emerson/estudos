af0815f46a5ebc515f56da61ab1caf7dd1da02ff
seileremerson <seiler.emerson@gmail.com>
2022-01-06 14:29:01 -0300

    Update script.js

---
+++ b7web/javascript/projetos/quiz/script.js
@@ -0,0 +1,15 @@
+//Initial Data
+let currentQuestion = 0;
+
+showQuestion();
+
+//
+function showQuestion() {
+    if(questions[currentQuestion]) {
+        let q = questions[currentQuestion]
+
+        document.querySelector()
+    } else {
+        //acabaram
+    }
+}
