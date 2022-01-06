ff2ed22eaf15590dfd7666b6cf5b19d64514a534
seileremerson <seiler.emerson@gmail.com>
2022-01-06 14:04:52 -0300

    update

--- b7web/javascript/projetos/quadro_desenho/script.js
+++ b7web/javascript/projetos/quadro_desenho/script.js
@@ -1,5 +1,8 @@
 // Initial Data
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
@@ -11,7 +14,7 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
-
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 // Functions
 function colorClickEvent(e) {
@@ -19,17 +22,44 @@ function colorClickEvent(e) {
     currentColor = color;
 
     document.querySelector('.color.active').classList.remove('active');
-    e.target.classList.add('active')
+    e.target.classList.add('active');
 }
 
-function mouseDownEvent() {
-
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+    
 }
 
-function mouseMoveEvent() {
-
+function mouseMoveEvent(e) {
+    if(canDraw) {
+        draw(e.pageX, e.pageY);
+    }
 }
 
 function mouseUpEvent() {
-    
+    canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+    console.log("testando")
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height)
 }
