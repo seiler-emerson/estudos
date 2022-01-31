9ddf405e6cb050f1b2eb833a46fcb507fa9d32b5
seileremerson <seiler.emerson@gmail.com>
2022-01-31 10:20:24 -0300

    estudos

---
+++ functions_1.js
@@ -0,0 +1,8 @@
+//DECLARETION OF FUNCTION
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+//VIEW 
+createPhrases()
---
+++ script.js
@@ -0,0 +1,27 @@
+//Object
+
+const person = {
+    name: 'João',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+//console.log(`${person.name} tem ${person.age} anos`)
+
+//Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat',
+    {
+        name: 'Cat',
+        age: 3
+    }
+]
+
+//console.log(animals[0])
+//console.log(animals.length)
+console.log(animals[3].name)
+
