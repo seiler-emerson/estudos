ddcab4761c23a98386c502b83f6ca6af70da5f17
seileremerson <seiler.emerson@gmail.com>
2022-02-01 10:17:27 -0300

    estudos

--- functions_1.js
+++
@@ -1,8 +0,0 @@
-//DECLARETION OF FUNCTION
-function createPhrases() {
-    console.log('Estudar é muito bom')
-    console.log('Paciência e persistência')
-    console.log('Revisão é a mãe do aprendizado')
-}
-//VIEW 
-createPhrases()
