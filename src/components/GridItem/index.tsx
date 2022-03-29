423be8caaf5200c02bce159d440576514eb427e7
Emerson <seiler.emerson@gmail.com>
2022-03-29 09:27:35 -0300

    first commit

---
+++ src/components/GridItem/index.tsx
@@ -0,0 +1,25 @@
+import { GridItemType } from '../../types/GridItemType';
+import * as C from './styles'
+import b7Svg from '../../svgs/b7.svg'
+import { items } from '../../data/items'
+
+type Props = {
+    item: GridItemType,
+    onClick: () => void
+}
+
+export const GridItem = ({item, onClick}: Props) => {
+    return (
+        <C.Container
+            shownBackground={item.permanentShown || item.shown}
+            onClick={onClick}
+        >
+            {item.permanentShown === false && item.shown === false &&
+                <C.Icon src={b7Svg} alt="" opacity={.1} />
+            }
+            {(item.permanentShown || item.shown) && item.item !== null &&
+                <C.Icon src={items[item.item].icon} alt="" />
+            }
+        </C.Container>
+    );
+}
