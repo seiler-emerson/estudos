74a7a84a33c2a61aac0a9636b4aa0fd541e1640c
Emerson <seiler.emerson@gmail.com>
2022-04-04 07:50:49 -0300

    React Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão na padaria.', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria.', done: false }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        
+        <AddArea />
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item} />
+        ))}
+
+      </C.Area>
+    </C.Container>
   );
 }
 
