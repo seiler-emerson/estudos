9c872d185f6027baa6dea700dc8b5ea5e8a2b66b
seiler-emerson <seiler.emerson@gmail.com>
2022-06-01 15:40:18 -0300

    update

--- script.js
+++ script.js
@@ -26,22 +26,43 @@ function closeMenuCompact() {
 // ================================================ //
 
 let totalProjects = document.querySelectorAll('.box').length;
-document.querySelector('.projects').style.width = `calc(340px * ${totalProjects})`;
+// document.querySelector('.projects').style.width = `calc(340px * ${totalProjects})`;
+let actualWidthDisplay = window.screen.width;
 
 let currentProject = 0;
 
 function goPrev() {
     currentProject--;
-    if (currentProject<0) {
-        currentProject = totalProjects - 3;
-    } 
+    if(actualWidthDisplay <= 800) {
+        if (currentProject<0) {
+            currentProject = totalProjects - 1;
+        } 
+    } else if(actualWidthDisplay <= 1200) {
+        if (currentProject<0) {
+            currentProject = totalProjects - 2;
+        } 
+    } else {
+        if (currentProject<0) {
+            currentProject = totalProjects - 3;
+        } 
+    }
     updateMargin();
 }
 
 function goNext() {
     currentProject++;
-    if (currentProject>(totalProjects - 3)) {
-        currentProject = 0;
+    if(actualWidthDisplay <= 800) {
+        if (currentProject>(totalProjects - 1)) {
+            currentProject = 0;
+        }
+    } else if(actualWidthDisplay <= 1200) {
+        if (currentProject>(totalProjects - 2)) {
+            currentProject = 0;
+        }
+    } else { 
+        if (currentProject>(totalProjects - 3)) {
+            currentProject = 0;
+        }
     }
     updateMargin();
 }
@@ -50,14 +71,12 @@ function goNext() {
 //480px
 function updateMargin() {
     let newMargin = 0;
-    let actualWidthDisplay = window.screen.width;
     if(actualWidthDisplay <= 480) {
         newMargin = (currentProject * 310);
     } else {
         newMargin = (currentProject * 340);
     }
     document.querySelector('.projects').style.marginLeft = `-${newMargin}px`;
-    console.log("cliquei")
 }
 
-setInterval(goNext, 4000)
+setInterval(goNext, 2000)
