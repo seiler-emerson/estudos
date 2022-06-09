fec1312e26daf274f5622b12c54341c3cac4d910
seiler-emerson <seiler.emerson@gmail.com>
2022-06-09 14:11:44 -0300

    update

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -7,13 +7,16 @@ public class Aula02 {
 		c1.cor = "Azul";
 		c1.ponta = 0.5f;
 		c1.destampar();
-		c1.rabiscar();
 		c1.status();
+		c1.rabiscar();
 		
 		Caneta c2 = new Caneta();
+		c2.modelo = "Hostnet";
+		c2.cor = "Preta";
+		c2.destampar();
+		c2.status();
+		c2.rabiscar();
 		
-		
-
 	}
 
 }
