575caf98a9839027177e88a11829abe73d54b8d2
seiler-emerson <seiler.emerson@gmail.com>
2022-06-09 14:01:30 -0300

    Initial commit

---
+++ Aula02/src/aula02/Aula02.java
@@ -0,0 +1,19 @@
+package aula02;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.cor = "Azul";
+		c1.ponta = 0.5f;
+		c1.destampar();
+		c1.rabiscar();
+		c1.status();
+		
+		Caneta c2 = new Caneta();
+		
+		
+
+	}
+
+}
