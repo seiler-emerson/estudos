f1ec351921fd7d7dbf8453399d23cffc231c7154
seiler-emerson <seiler.emerson@gmail.com>
2022-06-27 15:54:01 -0300

    Curso angular

--- angular/curso_angular/meu-primeiro-projeto/src/app/app.module.ts
+++ angular/curso_angular/meu-primeiro-projeto/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { DataBidingComponent } from './data-biding/data-biding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/curso_angular/meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ angular/curso_angular/meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -14,9 +14,20 @@ export class DataBidingComponent implements OnInit {
   public checkedDisable: boolean = false;
   public imgSrc: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png";
   public imgTitle: string = "Property Binding";
+  
+  public position: {x: number, y:number} = { x: 0, y:0 };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor)
   }
 
+  public mouseMoveTest(valor: MouseEvent) {
+    //console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
