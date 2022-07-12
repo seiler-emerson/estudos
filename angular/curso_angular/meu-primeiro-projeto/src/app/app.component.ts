0b2563aa6a6299fa5db4b7114c69ea9d3669696c
seiler-emerson <seiler.emerson@gmail.com>
2022-07-12 10:12:53 -0300

    App Calculadora Angular

--- angular/curso_angular/meu-primeiro-projeto/src/app/app.component.ts
+++ angular/curso_angular/meu-primeiro-projeto/src/app/app.component.ts
@@ -17,12 +17,16 @@ import { Component, OnInit } from '@angular/core';
   <app-input [contador]="addValue"></app-input>
   <br>
   <button (click)="add()">Add</button>
-  -->
+  
   <ng-template [ngIf]="getDados">
     <h1>{{getDados.nome}}</h1>
     <h2>{{getDados.idade}}</h2>
   </ng-template>
   <app-output (enviarDados)="setDados($event)"></app-output>
+  -->
+  
+  <app-food-add></app-food-add>
+  <app-food-list></app-food-list>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
---
+++ angular/curso_angular/meu-primeiro-projeto/src/app/services/food-list.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+
+  private list: Array<string> = [
+    "X Bacon",
+    "Feijão",
+    "Ovo"
+  ];
+
+  constructor() { }
+
+  public foodList() {
+    return this.list;
+  }
+
+  public foodListAdd(value: string) {
+    return this.list.push(value);
+  }
+}
