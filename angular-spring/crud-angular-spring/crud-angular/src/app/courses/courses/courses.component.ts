1e53cf58eeabad7abac079d8d648f0f53934a3f7
seiler-emerson <seiler.emerson@gmail.com>
2022-08-16 13:22:15 -0300

    study angular

---
+++ angular-spring/crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,28 @@
+
+import { Component, OnInit } from '@angular/core';
+
+import { Course } from './../model/course';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [ ];
+  displayedColumns = ['name', 'category']
+
+  // coursesService: CoursesService;
+
+
+  constructor(private coursesService: CoursesService) {
+    // this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-spring/crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Course } from './../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  list(): Course[] {
+
+    return [
+      { _id: '1', name: 'Angular', category: 'front-end'}
+    ]
+  }
+}
