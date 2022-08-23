32db9197734d963e301903e1b5e00a64166eff96
seiler-emerson <seiler.emerson@gmail.com>
2022-08-23 16:37:24 -0300

    study springboot

---
+++ springboot/vendas/src/test/java/io/githun/seileremerson/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.githun.seileremerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
