ae5a5b79e33599955401109968962f90e5f4ef4b
seiler-emerson <seiler.emerson@gmail.com>
2022-08-28 19:46:10 -0300

    Angular Study

--- angular/angular-loiane/crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ angular/angular-loiane/crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,9 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -8,16 +12,34 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category']
 
-  constructor() {
+  // CoursesService: CoursesService;
+
+  constructor(
+    private CoursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [];
+    //this.CoursesService = new CoursesService();
+    this.courses$ = this.CoursesService.list().pipe(
+      catchError(error => {
+        this.onError("Erro ao carregar cursos.");
+
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
+
   }
 
 }
--- angular/angular-loiane/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ angular/angular-loiane/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class AppMaterialModule { }
