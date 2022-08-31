ef9f1b8e4497eb2068783cd14205ce19235d7ceb
seiler-emerson <seiler.emerson@gmail.com>
2022-08-31 10:09:09 -0300

    CRUD Angular

--- angular/angular-loiane/crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ angular/angular-loiane/crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -29,11 +31,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => this.onError());
+    .subscribe(result => this.onSuccess(), error => this.onError());
+    this.onCancel()
   }
 
   onCancel() {
+    this.location.back()
+  }
 
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!','', { duration: 5000})
   }
 
   private onError() {
--- angular/angular-loiane/crud-angular-spring/crud-spring/src/main/java/com/emerson/controller/CourseController.java
+++ angular/angular-loiane/crud-angular-spring/crud-spring/src/main/java/com/emerson/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.emerson.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emerson.model.Course;
@@ -18,9 +23,19 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
 
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
